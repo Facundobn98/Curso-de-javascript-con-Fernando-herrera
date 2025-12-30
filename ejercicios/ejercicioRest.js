c2d699d6ae353738e46086db26be6b0540074115
facundobn98 <facundobrun1998@gmail.com>
2025-12-29 22:39:32 -0300

    ejercicios

--- ejercicios/ejercicioRest.js
+++ ejercicios/ejercicioRest.js
@@ -29,7 +29,7 @@ numeroGrande(1,2,3,4,5,6,7,8,9)
 //ejercicio 4
 
 const numeroChico = (...numeros) => {
-    let numero = 0;
+    let numero = numeros[0];
     for (let i = 0; i < numeros.length; i++){
         if (numeros[i] < numero){
             numero = numeros[i]
