937ce7a8eeed8ea14e66dc44342dd060611b5ad3
facundobn98 <facundobrun1998@gmail.com>
2026-01-03 02:33:26 -0300

    empezando con if/else y else if

--- fundamentos/if-else.js
+++ fundamentos/if-else.js
@@ -1,7 +1,35 @@
 let a = 15;
 
-if ( a>10 ){
+if ( a>10 ){ // undefined, null, una asignacion
     console.log('A es mayor a 10')
 }
+else {
+    console.log('es menor a 10')
+}
 
-console.log('fin del programa')
+//console.log('fin del programa')
+
+const hoy = new Date();
+let dia = hoy.getDate(); //0: Domingo, 1: lunes, 2: martes, 3: Miercoles...
+
+console.log({ dia })
+
+if (dia === 0){
+    console.log('es Domigon')
+}
+else {
+    if ( dia === 1){
+        console.log('no es domingo ni lunes')
+    }
+    console.log('no es domingo')
+}
+// = asignacion
+// == igual
+// === identico
+
+if (dia === 0){
+    console.log('es Domigon')
+}
+else  {
+    console.log('no es domingo')
+}
