e37882edd01e36d01ae960d15e0f1c401caa53e2
facundobn98 <facundobrun1998@gmail.com>
2026-01-06 17:34:12 -0300

    practicando la logica booleana

--- ejercicios/ejercicioIfElseOr.js
+++ ejercicios/ejercicioIfElseOr.js
@@ -4,6 +4,47 @@
 
 let edad = 10;
 
-if (edad > 12 || edad > 65){
+if (edad < 12 || edad > 65){
     console.log('podes pasar')
+}
+
+// Mostrar
+// “Día libre” si el día es sábado O domingo.
+
+
+let dia2 = 'sabado';
+
+if (dia2 == 'sabado' || dia2 == 'domingo'){
+    console.log('dia libre')
+}
+
+let temperatura = 5;
+
+if (temperatura < 10 || temperatura < 30){
+    console.log('llevar abrigo')     
+}
+//2
+// mostrar 'numero valido' si es numero 0 o 100
+
+let numero = 100;
+
+if (numero === 0 || numero === 100) {
+    console.log('numero valido');
+}
+//
+//Mostrar “Acceso permitido”
+//si el usuario es admin O moderador.
+
+let rol = "admin";
+
+if( rol === 'admin'|| rol === 'moderador'){
+    console.log('acceso permitido')
+}
+//Aplicar descuento si el precio
+//es menor a 1000 O mayor a 5000.
+
+let precio = 6000;
+
+if (precio < 1000 || precio > 5000){
+    console.log('descuento aceptado')
 }
