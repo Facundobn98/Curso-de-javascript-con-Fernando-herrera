6924bb8d248bc2070db30711dbd54996ea978bed
facundobn98 <facundobrun1998@gmail.com>
2026-01-13 15:07:30 -0300

    ejercicios con while

---
+++ ejercicios/ejercicioWhile.js
@@ -0,0 +1,22 @@
+//ejercicios con while
+//Mostrá por consola los números del 1 al 10 usando while.
+let numero = 0;
+
+while ( numero <= 9){
+    numero = numero + 1
+    console.log(numero)
+}
+//2
+//Mostrá solo los números pares entre 0 y 20.
+let numero2 = 20;
+
+while ( numero2 >= 1 ){
+    numero2 = numero2 - 1;
+    console.log(numero2)
+}
+
+let contador = 0;
+
+while (  ){
+    
+}
