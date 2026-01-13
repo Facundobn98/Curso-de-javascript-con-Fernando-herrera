6d9636554b0fb7ea7f0dfabc4a527350389efb09
facundobn98 <facundobrun1998@gmail.com>
2026-01-12 21:35:46 -0300

    praticando lo visto otra vez

--- ejercicios/ejercicioBucles.js
+++ ejercicios/ejercicioBucles.js
@@ -19,7 +19,25 @@ for (let i = numeros2.length - 1; i>=0;i--) {
 let numeros3 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 for (let i = 0;i<numeros3.length; i++){
-    if (numeros % 2 === 0){
+    if (numeros3[i] % 2 === 0){
         console.log(numeros3[i])
+    }
+}
+// 4
+// Contar hasta un número dado
+
+let limite = 7;
+
+for (let i = 1; i <= limite; i++) {
+    console.log(i)
+}
+//5
+//Del 1 al 10, mostrá solo los números mayores a 5.
+
+let numeros4 = [1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 0; i < numeros4.length;i++){
+    if (numeros4[i] > 5){
+        console.log(numeros4[i])
     }
 }
