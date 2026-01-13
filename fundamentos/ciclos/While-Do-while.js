c04a59ff4252da0df7067a5a6db6df010ee96a43
facundobn98 <facundobrun1998@gmail.com>
2026-01-12 21:54:41 -0300

    ciclos y estructuras de control(while/DoWhile

---
+++ fundamentos/ciclos/While-Do-while.js
@@ -0,0 +1,30 @@
+console.warn('while')
+const autos = ['ford','honda','toyota','amarok']
+
+let i = 0;
+
+while ( i<autos.length ){
+    console.log( carros[i] )
+    // i = i + 1;
+    i++;
+}
+// undefined
+// null
+// false
+
+while ( autos[i] ){
+    if ( i=== 1){
+        //break;
+        i++;
+        continue;
+    }
+    console.log( autos[i] );
+    i++;
+}
+
+console.warn('Do while')
+let j = 0;
+do {
+    console.log(autos[j]);
+    j++;
+}while( carros[j] );
