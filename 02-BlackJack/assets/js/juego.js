0bbfdcfdd077b584c9b045b25223cb556f03cc9f
facundobn98 <facundobrun1998@gmail.com>
2026-01-16 12:32:03 -0300

    seguimiento del juego de BlackJack

--- 02-BlackJack/assets/js/juego.js
+++ 02-BlackJack/assets/js/juego.js
@@ -6,12 +6,20 @@
  */
 
 let deck = [];
+const tipos = ['C','D','H','S']
+const especiales = ['A','J','Q','K']
+
 
 const crearDeck = (  ) => {
     for( let i = 2;i <= 10; i++ ) {
-        deck.push( i + 'C')
+        for(let tipo of tipos) {
+            deck.push( i + tipo);
+        }
     }
-
+    for(let tipo of tipos){
+        for( let especial of especiales)
+            deck.push ( especial + tipo);
+    }
     console.log( deck )
 }
 
