822229ab269b993ea5033274ec1fd5a4127bff87
facundobn98 <facundobrun1998@gmail.com>
2026-01-17 22:38:35 -0300

    notas de informacion importante

--- 02-BlackJack/assets/js/juego.js
+++ 02-BlackJack/assets/js/juego.js
@@ -5,22 +5,51 @@
  * 2s = two of spades 
  */
 
+// Creamos un array vacío donde vamos a guardar todas las cartas
 let deck = [];
-const tipos = ['C','D','H','S']
-const especiales = ['A','J','Q','K']
 
+// Palos de las cartas:
+// C = Clubs (Tréboles)
+// D = Diamonds (Diamantes)
+// H = Hearts (Corazones)
+// S = Spades (Picas)
+const tipos = ['C','D','H','S'];
 
+// Cartas especiales (las que no son numéricas)
+const especiales = ['A','J','Q','K'];
+
+// Función que crea el mazo de cartas
 const crearDeck = (  ) => {
-    for( let i = 2;i <= 10; i++ ) {
+
+    // Primer FOR:
+    // Recorre los números del 2 al 10
+    for( let i = 2; i <= 10; i++ ) {
+
+        // Segundo FOR:
+        // Recorre cada tipo (palo)
         for(let tipo of tipos) {
+
+            // Concatenamos el número con el tipo
+            // Ejemplo: 2C, 5H, 10S
             deck.push( i + tipo);
         }
     }
+
+    // Segundo bloque:
+    // Recorremos los tipos nuevamente
     for(let tipo of tipos){
+
+        // Recorremos las cartas especiales
         for( let especial of especiales)
+
+            // Concatenamos la carta especial con el tipo
+            // Ejemplo: AC, KD, QH
             deck.push ( especial + tipo);
     }
+
+    // Mostramos el deck completo en consola
     console.log( deck )
 }
 
-crearDeck(  ); 
+// Llamamos a la función para que se ejecute
+crearDeck(  );
