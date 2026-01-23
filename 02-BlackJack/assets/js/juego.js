4a91c604da42606f59c5b73910bb50076f011aef
facundobn98 <facundobrun1998@gmail.com>
2026-01-23 19:29:25 -0300

    logica y turno de la computadora

--- 02-BlackJack/assets/js/juego.js
+++ 02-BlackJack/assets/js/juego.js
@@ -1,76 +1,35 @@
-/**
- * 2c = two of clubs
- * 2d = two of diaminds 
- * 2h = two of hearts 
- * 2s = two of spades 
- */
-
-// Creamos un array vacío donde vamos a guardar todas las cartas
 let deck = [];
 
-// Palos de las cartas:
-// C = Clubs (Tréboles)
-// D = Diamonds (Diamantes)
-// H = Hearts (Corazones)
-// S = Spades (Picas)
 const tipos = ['C','D','H','S'];
-
-// Cartas especiales (las que no son numéricas)
 const especiales = ['A','J','Q','K'];
 
 let puntosJugador = 0;
-    puntosComputadora = 0;
+let puntosComputadora = 0;
 
-// referencias del HTML
-const btnPedir = document.querySelector('#btnPedir')
-
-
+const btnPedir = document.querySelector('#btnPedir');
 
 const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector('#cartas-computadora');
 const puntosHTML = document.querySelectorAll('small');
 
+const crearDeck = () => {
 
-
-// Función que crea el mazo de cartas
-const crearDeck = (  ) => {
-
-    // Primer FOR:
-    // Recorre los números del 2 al 10
     for( let i = 2; i <= 10; i++ ) {
-
-        // Segundo FOR:
-        // Recorre cada tipo (palo)
         for(let tipo of tipos) {
-
-            // Concatenamos el número con el tipo
-            // Ejemplo: 2C, 5H, 10S
             deck.push( i + tipo);
         }
     }
 
-    // Segundo bloque:
-    // Recorremos los tipos nuevamente
     for(let tipo of tipos){
-
-        // Recorremos las cartas especiales
         for( let especial of especiales)
-
-            // Concatenamos la carta especial con el tipo
-            // Ejemplo: AC, KD, QH
             deck.push ( especial + tipo);
     }
 
-    // Mostramos el deck completo en consola
-    //console.log( deck );
     deck = _.shuffle( deck );
-    
     return deck;
 }
 
-// Llamamos a la función para que se ejecute
-crearDeck(  );
-
-// esta funcion me permite tomar una carta
+crearDeck();
 
 const pedirCarta = () => {
 
@@ -79,32 +38,36 @@ const pedirCarta = () => {
     }
 
     const carta = deck.pop();
-
     return carta;
 }
 
-
-    //pedirCarta( );
 const valorCarta = ( carta ) => {
-    //codigo corto
+
     const valor = carta.substring(0, carta.length - 1);
+
     return  ( isNaN( valor ) ) ?
-            ( valor === 'A' )   ? 11 : 10
+            ( valor === 'A' ) ? 11 : 10
             : valor * 1;
-
-
-    //Codigo largo
-    //let puntos = 0;
-    // 2 = 2, 10 = 10, 3 = 3
-    //if ( isNaN( valor ) ) {    
-    //    puntos = ( valor === 'A') ? 11:10;
-    //} else {
-    //    console.log('es un numero')
-    //    puntos = valor * 1;
-    //}
-    //console.log(puntos)
 }
-//  Eventos
+
+const turnoComputadora = ( puntosMinimos ) =>{
+    do {
+    const carta = pedirCarta();
+
+    puntosComputadora = puntosComputadora + valorCarta( carta );
+    puntosHTML[1].innerText = puntosComputadora;
+
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${ carta }.png`;
+    imgCarta.classList.add('carta');
+    divCartasComputadora.append( imgCarta ); 
+
+    if (puntosMinimos > 21){
+        break;
+    }
+}while ( (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21) );
+
+}
 
 btnPedir.addEventListener('click', () => {
 
@@ -119,11 +82,12 @@ btnPedir.addEventListener('click', () => {
     divCartasJugador.append( imgCarta );
 
     if (puntosJugador > 21){
-        console.warn('perdiste')
         btnPedir.disabled = true
-    }else if (puntosJugador === 21){
-        console.warn('ganaste')
-    }
+        turnoComputadora(puntosJugador);
 
+    } else if (puntosJugador === 21){
+        console.warn('perdite')
+        btnPedir.disabled = true;
+        turnoComputadora(puntosJugador)
+    } 
 });
-
