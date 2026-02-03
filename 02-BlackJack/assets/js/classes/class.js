5df2f828f41727b9a1c673e9bfaa52ea85f88f0c
facundobn98 <facundobrun1998@gmail.com>
2026-02-03 12:32:10 -0300

    haciendo mis propios metodos personalizados

--- 02-BlackJack/assets/js/classes/class.js
+++ 02-BlackJack/assets/js/classes/class.js
@@ -11,9 +11,22 @@ class Persona {
             this.codigo = codigo;
             this.frase = frase;
         }
+        quienSoy(){
+            console.log(`soy ${this.nombre} y mi identidad es ${this.codigo}`);
+        }
+        miFrase(){
+            this.quienSoy();
+            console.log(`mi frase es ${this.frase}`)
+        }
+
 }
 
 const spiderman = new Persona('peter','spiderman','hola');
 const ironman = new Persona('tony','ironman','hola');
-console.log( spiderman )
-console.log( ironman )
+console.log( spiderman );
+console.log( ironman );
+
+spiderman.quienSoy();
+ironman.quienSoy();
+
+spiderman.miFrase ();
