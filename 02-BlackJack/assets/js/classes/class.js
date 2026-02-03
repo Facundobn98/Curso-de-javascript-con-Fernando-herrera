4b9b6aefcd34ecd2ba78b61489fe4178782621a5
facundobn98 <facundobrun1998@gmail.com>
2026-02-03 14:36:48 -0300

    set/get

--- 02-BlackJack/assets/js/classes/class.js
+++ 02-BlackJack/assets/js/classes/class.js
@@ -4,12 +4,18 @@ class Persona {
     nombre = '';
     codigo = '';
     frase = '';
-
-
+    comida = '';
         constructor(nombre,codigo,frase){
             this.nombre = nombre;
             this.codigo = codigo;
             this.frase = frase;
+        }
+
+        set setComidaFavorita(comida){
+            this.comida = comida;
+        }
+        get getComidaFavorita(){
+            return `la comida favorita de ${ this.nombre } es ${ this.comida}`;
         }
         quienSoy(){
             console.log(`soy ${this.nombre} y mi identidad es ${this.codigo}`);
@@ -18,15 +24,16 @@ class Persona {
             this.quienSoy();
             console.log(`mi frase es ${this.frase}`)
         }
-
 }
 
 const spiderman = new Persona('peter','spiderman','hola');
 const ironman = new Persona('tony','ironman','hola');
-console.log( spiderman );
 console.log( ironman );
 
 spiderman.quienSoy();
-ironman.quienSoy();
 
-spiderman.miFrase ();
+spiderman.setComidaFavorita = 'sanguche de milanesa';
+// spiderman.comida = 'epstein'
+
+
+console.log( spiderman );
