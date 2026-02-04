11762ea193ea5a01b08ed5ac4a432f9296332bc8
facundobn98 <facundobrun1998@gmail.com>
2026-02-04 16:57:57 -0300

    ejercicios de class/get/set

---
+++ 01-fundamentos/ejercicios/ejercicios-class-getters-setters-metodos.js
@@ -0,0 +1,67 @@
+// class Animal {
+//     nombre;
+//     tipo;
+//     sonido;
+
+//     constructor(nombre, tipo, sonido) {
+//         this.nombre = nombre;
+//         this.tipo = tipo;
+//         this.sonido = sonido;
+//     }
+
+//     presentarse() {
+//         console.log(
+//             `Hola, soy un ${this.tipo} llamado ${this.nombre} y produzco el sonido ${this.sonido}`
+//         );
+//     }
+// }
+// const firulais = new Animal('Firulais', 'perro', 'ladrido');
+// firulais.presentarse();
+//
+class Animal {
+    nombre;
+    tipo;
+    sonido;
+
+    constructor(nombre, tipo, sonido) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.sonido = sonido;
+    }
+
+    presentarse() {
+        console.log(
+            `Hola, soy un ${this.tipo} llamado ${this.nombre} y produzco el sonido ${this.sonido}`
+        );
+    }
+}
+const perro = new Animal('Firulais', 'perro', 'ladrido');
+const gato = new Animal('timo', 'perro', 'maullido');
+const loro = new Animal('ramon', 'perro', 'lenguaje humano...');
+const animales = [perro,gato,loro]
+animales.forEach(animal => {
+    animal.presentarse();
+});
+//
+class Usuario {
+    nombre;
+    rol;
+    constructor(nombre,rol){
+        this.nombre = nombre;
+        this.rol = rol;
+    }
+    set setRol(nuevoRol){
+        if (nuevoRol === 'admin' || nuevoRol === 'user'){
+            this.rol = nuevoRol;
+        }
+        else {
+            console.error('rol invalido')
+        }
+    }
+    get info(){
+      return `Usuario: ${this.nombre} | Rol: ${this.rol}`  
+    }
+}
+const u = new Usuario('facundo','admin');
+u.setRol =
+console.log(u.info);
